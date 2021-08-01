e27d3f858d40fc18a19ca94e10b3d641586aad56
Jeffrey <jeffrey@spacemaker.ai>
2021-07-31 21:38:12 -0600

    feat: full board with pieces

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import React, { setState } from "react";
 import styled, { css } from "styled-components";
-import { DraggablePiece, BlackPieces } from "./pieces";
+import { DraggablePiece, BluePieces, GreenPieces } from "./pieces";
 
 const Form = styled.div`
   position: absolute;
 `;
+
+export function getIndexPieceMap() {
+    var map = new Map();
+    map.set(0, GreenPieces.rook)
+    map.set(1, GreenPieces.knight)
+    map.set(2, GreenPieces.bishop)
+    map.set(3, GreenPieces.queen)
+    map.set(4, GreenPieces.king)
+    map.set(5, GreenPieces.bishop)
+    map.set(6, GreenPieces.knight)
+    map.set(7, GreenPieces.rook)
+    map.set(8, GreenPieces.pawn)
+    map.set(9, GreenPieces.pawn)
+    map.set(10, GreenPieces.pawn)
+    map.set(11, GreenPieces.pawn)
+    map.set(12, GreenPieces.pawn)
+    map.set(13, GreenPieces.pawn)
+    map.set(14, GreenPieces.pawn)
+    map.set(15, GreenPieces.pawn)
+
+    map.set(63, BluePieces.rook)
+    map.set(62, BluePieces.knight)
+    map.set(61, BluePieces.bishop)
+    map.set(60, BluePieces.king)
+    map.set(59, BluePieces.queen)
+    map.set(58, BluePieces.bishop)
+    map.set(57, BluePieces.knight)
+    map.set(56, BluePieces.rook)
+    map.set(55, BluePieces.pawn)
+    map.set(54, BluePieces.pawn)
+    map.set(53, BluePieces.pawn)
+    map.set(52, BluePieces.pawn)
+    map.set(51, BluePieces.pawn)
+    map.set(50, BluePieces.pawn)
+    map.set(49, BluePieces.pawn)
+    map.set(48, BluePieces.pawn)
+    return map;
+}
 
 const Board = styled.div`
   border-style: solid;
@@ -37,9 +75,21 @@ export function renderSquare(i) {
   let isBlack =
     (Math.floor(i / 8) % 2 == 0 && i % 2 == 0) ||
     (Math.floor(i / 8) % 2 == 1 && i % 2 == 1);
+
+  const indexPieceMap = getIndexPieceMap();
   if (isBlack) {
+      if (indexPieceMap.has(i)) {
+          return (<Square id={i}>
+              <DraggablePiece piece={indexPieceMap.get(i)}></DraggablePiece>
+          </Square>)
+      }
     return <Square id={i}></Square>;
   }
+  if (indexPieceMap.has(i)) {
+    return (<Square Black id={i}>
+        <DraggablePiece piece={indexPieceMap.get(i)}></DraggablePiece>
+    </Square>)
+}
   return <Square Black id={i}></Square>;
 }
 
@@ -75,10 +125,6 @@ class App extends React.Component {
     return (
       <Form>
         <Board>{this.items}</Board>
-        <DraggablePiece piece={BlackPieces.pawn}></DraggablePiece>
-        <DraggablePiece piece={BlackPieces.rook}></DraggablePiece>
-        <DraggablePiece piece={BlackPieces.bishop}></DraggablePiece>
-        <DraggablePiece piece={BlackPieces.knight}></DraggablePiece>
       </Form>
     );
   }
@@ -86,9 +132,6 @@ class App extends React.Component {
 
 export default App;
 /*
-<Board>
-                {this.items}
-            </Board>
 This is a default export. This means that, when importing, we must import like...
 
 import App from "./App";
